e36d5fc05a7dfc2c8a27a52ada765222140461b4
J7mbo <jm_cj@hotmail.com>
2013-03-08 15:24:26 +0000

    initial release

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+require_once('TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
+);
+
+/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+$url = 'https://api.twitter.com/1.1/blocks/create.json';
+$requestMethod = 'POST';
+
+/** POST fields required by the URL above. See relevant docs as above **/
+$postfields = array(
+    'screen_name' => 'usernameToBlock', 
+    'skip_status' => '1'
+);
+
+/** Perform the request and echo the response **/
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod)
+         ->setPostfields($postfields)
+         ->performRequest();
+
